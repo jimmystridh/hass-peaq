554d4a77fff8e1db8b22a467b160a24ea4b4c566
Magnus Eldén <magnus.elden@outlook.com>
2022-07-13 22:08:59 +0200

    fix more parts of keba. still wip

--- custom_components/peaqev/peaqservice/chargertypes/types/keba.py
+++ custom_components/peaqev/peaqservice/chargertypes/types/keba.py
@@ -1,13 +1,17 @@
 import logging
 
 from homeassistant.core import HomeAssistant
+import time
+from peaqevcore.chargertype_service.models.servicecalls_dto import ServiceCallsDTO
+from peaqevcore.chargertype_service.models.servicecalls_options import ServiceCallsOptions
 from peaqevcore.models.chargerstates import CHARGERSTATES
 
-from custom_components.peaqev.peaqservice.chargertypes.calltype import CallType
-from custom_components.peaqev.peaqservice.chargertypes.chargerbase import ChargerBase
+from peaqevcore.chargertype_service.chargertype_base import ChargerBase
+from peaqevcore.chargertype_service.models.calltype import CallType
 from custom_components.peaqev.peaqservice.util.constants import (
     CURRENT,
 )
+import custom_components.peaqev.peaqservice.chargertypes.entitieshelper as helper
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,7 +19,8 @@ ENTITYENDINGS = []
 NATIVE_CHARGERSTATES = []
 DOMAINNAME = "keba"
 UPDATECURRENT = True
-#docs: https://github.com/kirei/hass-chargeamps
+UPDATECURRENT_ON_TERMINATION = True #might need to change this one.
+#docs: https://github.com/home-assistant/core/tree/dev/homeassistant/components/keba
 
 
 class Keba(ChargerBase):
@@ -23,56 +28,73 @@ class Keba(ChargerBase):
         super().__init__(hass)
         self._hass = hass
         self._chargerid = chargerid
-        self._chargeamps_connector = 1
-        self._domainname = DOMAINNAME
-        self._entityendings = ENTITYENDINGS
+        self.options.powerswitch_controls_charging = False
+        self.domainname = DOMAINNAME
+        self.entities.imported_entityendings = ENTITYENDINGS
+        self.native_chargerstates = NATIVE_CHARGERSTATES
+        self.chargerstates[CHARGERSTATES.Idle] = []
+        self.chargerstates[CHARGERSTATES.Connected] = []
+        self.chargerstates[CHARGERSTATES.Charging] = []
+        self.chargerstates[CHARGERSTATES.Done] = []
 
-        self._native_chargerstates = NATIVE_CHARGERSTATES
-        self._chargerstates[CHARGERSTATES.Idle] = []
-        self._chargerstates[CHARGERSTATES.Connected] = []
-        self._chargerstates[CHARGERSTATES.Charging] = []
+        entitiesobj = helper.getentities(
+            self._hass,
+            helper.EntitiesPostModel(
+                self.domainname,
+                self.entities.entityschema,
+                self.entities.imported_entityendings
+            )
+        )
+        self.entities.imported_entities = entitiesobj.imported_entities
+        self.entities.entityschema = entitiesobj.entityschema
 
-        self.getentities()
         self.set_sensors()
 
-        servicecall_params = {}
-        servicecall_params[CURRENT] = "current"
-
-        _on_off_params = { }
-
-        _on = CallType("enable", _on_off_params)
-        _off = CallType("disable", _on_off_params)
+        _on = CallType("enable", {})
+        _off = CallType("disable", {})
 
         self._set_servicecalls(
             domain=DOMAINNAME,
-            on_call=_on,
-            off_call=_off,
-            allowupdatecurrent= UPDATECURRENT,
-            update_current_call="set_current",
-            update_current_params=servicecall_params
+            model=ServiceCallsDTO(
+                on=_on,
+                off=_off,
+                update_current=CallType("set_current", {CURRENT: "current"})
+            ),
+            options=ServiceCallsOptions(
+                allowupdatecurrent=UPDATECURRENT,
+                update_current_on_termination=UPDATECURRENT_ON_TERMINATION
+            )
         )
 
+    def getentities(self, domain: str = None, endings: list = None):
+        if len(self.entities.entityschema) < 1:
+            domain = self.domainname if domain is None else domain
+            endings = self.entities.imported_entityendings if endings is None else endings
+
+            entities = helper.get_entities_from_hass(self._hass, domain)
+
+            if len(entities) < 1:
+                _LOGGER.error(f"no entities found for {domain} at {time.time()}")
+            else:
+                _endings = endings
+                candidate = ""
+
+                for e in entities:
+                    splitted = e.split(".")
+                    for ending in _endings:
+                        if splitted[1].endswith(ending):
+                            candidate = splitted[1].replace(ending, '')
+                            break
+                    if len(candidate) > 1:
+                        break
+
+                self.entities.entityschema = candidate
+                _LOGGER.debug(f"entityschema is: {self.entities.entityschema} at {time.time()}")
+                self.entities.imported_entities = entities
+
     def set_sensors(self):
-         self.powermeter = f"sensor.{self._entityschema}_charging_power"
-         self.powerswitch = f"binary_sensor.{self._entityschema}_state"
+         self.entities.powermeter = f"sensor.{self.entities.entityschema}_charging_power"
+         self.entities.powerswitch = f"binary_sensor.{self.entities.entityschema}_state"
          #self.chargerentity = f"sensor.{self._entityschema}_1"
-         self.ampmeter = f"sensor.{self._entityschema}_max_current"
-         self.ampmeter_is_attribute = False
-
-    def _determine_entities(self):
-        ret = []
-        for e in self._entities:
-            entity_state = self._hass.states.get(e)
-            if entity_state != "unavailable":
-                ret.append(e)
-        return ret
-
-
-    def _determine_switch_entity(self):
-        ent = self._determine_entities()
-        for e in ent:
-            if e.startswith("switch."):
-                amps = self._hass.states.get(e).attributes.get("max_current")
-                if isinstance(amps, int):
-                    return e
-        raise Exception
+         self.entities.ampmeter = f"sensor.{self.entities.entityschema}_max_current"
+         self.options.ampmeter_is_attribute = False
